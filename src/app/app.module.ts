e84b627d5271877ab0b4b4afebdc12c42c04f354
amira Mohamed <amirassaber2018@gmail.com>
2022-01-21 20:00:13 +0200

    lab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { ProductItemComponent } from './core/product-feature/product-item/product-item.component';
+import { ProductListingComponent } from './core/product-feature/product-listing/product-listing.component';
+import { ProductFilterComponent } from './core/product-feature/product-filter/product-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductItemComponent,
+    ProductListingComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
